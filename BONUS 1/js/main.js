a4ad4cb2c6b800b7deacb48957cb14a4e333b39d
nicolo fossati <nicofossa01@gmail.com>
2023-03-13 14:29:08 +0100

    BONUS 1

---
+++ BONUS 1/js/main.js
@@ -0,0 +1,199 @@
+/* 
+	BONUS
+	1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale 
+	un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici 
+	compresi tra 0 e 9 e A e F.
+*/
+
+const data = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const mainDom = document.querySelector(".cards-container");
+
+change_color(data);
+print_array(data);
+
+const selectDom = document.querySelector('#choose');
+
+selectDom.addEventListener('change', function(){
+    const value = this.value;
+    const dataFiltered = data.filter((obj) => {
+        if(value == "all"){
+            return true;
+        } else if(obj.type == value){
+            return true;
+        }
+        return false;
+    });
+    print_array(dataFiltered);
+});
+
+function print_array(array){
+    mainDom.innerHTML = "";
+    array.forEach(obj => {
+        const newCardDom = document.createElement('div');
+        newCardDom.classList.add('card');
+        const iconDom = document.createElement('i');
+        iconDom.classList.add(`fa-solid`);
+        iconDom.classList.add(`${obj.prefix}${obj.name}`);
+        iconDom.style.color = `${obj.color}`;
+        const spanDom = document.createElement('span');
+        newCardDom.appendChild(iconDom);
+        newCardDom.appendChild(spanDom);
+        spanDom.innerHTML = `${obj.name.toUpperCase()}`;
+        mainDom.appendChild(newCardDom);
+    });
+}
+
+function change_color(array){
+	array.forEach(obj => {
+		obj.color = random_color();		
+	});
+}
+
+function random_color(){
+	let color = "#";
+	for(let i=0; i<6; i++){
+		color += random_chart();
+	}
+	return color;
+}
+
+function random_chart(){
+	const bool = random_includes(0,1);
+	if(bool){ //1
+		const af = random_includes(1,6);
+
+		if(af==1){
+			return "A";
+		} else if(af==2){
+			return "B";
+		} else if(af==3){
+			return "C";
+		} else if(af==4){
+			return "D";
+		} else if(af==5){
+			return "E";
+		} else if(af==6){
+			return "F";
+		}
+	} else { //0
+		return random_includes(0,9);
+	}
+}
+
+function random_includes(min, max){
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
